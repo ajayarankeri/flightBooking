66f8aa91e8344886d5e0c4bc10be6104f64c340d
vinayak <vinayakdesaimca@gmail.com>
2019-07-18 11:05:58 +0530

    add new code

--- src/main/java/com/hcl/flightReservation/config/SwaggerConfig.java
+++ src/main/java/com/hcl/flightReservation/config/SwaggerConfig.java
@@ -1,5 +1,5 @@
-package com.bank.InternetBanking.ConfigFiles;
-
+package com.hcl.flightReservation.config;
+g
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
