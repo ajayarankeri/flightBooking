6932332e6ec0e5ac58ef738b67fe5d953a77e35d
rksiet2 <rksiet2@gmail.com>
2019-07-18 13:56:57 +0530

    change in LoginController

--- src/main/java/com/hcl/flightReservation/controller/LoginController.java
+++ src/main/java/com/hcl/flightReservation/controller/LoginController.java
@@ -26,17 +26,16 @@ import com.hcl.flightReservation.exception.InvalidUserDataException;
 @RestController
 @RequestMapping("/user")
 public class LoginController {
-	
+
 	private static final String MANDETORY_ERR_MSG= "Mandetory element missing : "; 
 
 	@PostMapping("/login")
 	public ResponseEntity<?> loginUser(@RequestParam(value = "username") String username,
 			@RequestParam(value = "password") String password) {
-		
-		try {
+	try {
 			validateMandatoryElements(username,password);
-			System.out.println("login sucssfully");
-			
+			System.out.println("User logged In sucessfully");
+
 		} catch (InvalidUserDataException invalid) {
 			return new ResponseEntity<>(invalid.getMessage(), HttpStatus.BAD_REQUEST);
 		} catch(Exception e) {
@@ -47,14 +46,14 @@ public class LoginController {
 
 	// This function will check mandatory elements for login
 	private void validateMandatoryElements(String username, String password) throws InvalidUserDataException {
-		
+
 		if(ObjectUtils.isEmpty(username)) {
 			throw new InvalidUserDataException(MANDETORY_ERR_MSG+" userName");
 		}
-		
+
 		if(ObjectUtils.isEmpty(password)) {
 			throw new InvalidUserDataException(MANDETORY_ERR_MSG+" password");
 		}
-		
+
 	}
 }
