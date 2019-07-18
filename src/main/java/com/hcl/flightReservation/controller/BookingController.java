7ff3c8e6c90ab0419b426f2f21cf1e0645473c36
vinayak <vinayakdesaimca@gmail.com>
2019-07-18 13:21:54 +0530

    resolved exception

--- src/main/java/com/hcl/flightReservation/controller/BookingController.java
+++ src/main/java/com/hcl/flightReservation/controller/BookingController.java
@@ -1,5 +1,30 @@
 package com.hcl.flightReservation.controller;
 
-public class BookingController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.hcl.flightReservation.entity.Booking;
+import com.hcl.flightReservation.service.BookingService;
+
+@RestController
+@RequestMapping("/booking")
+public class BookingController {
+	@Autowired
+	BookingService bookingService;
+
+	@PostMapping("")
+	public ResponseEntity<Object> saveBooking(@RequestBody Booking booking){
+		
+		
+		
+		bookingService.saveBooking(booking);
+		
+		return new ResponseEntity<> ("record saved sucessfully",HttpStatus.OK);
+		
+	}
 }
